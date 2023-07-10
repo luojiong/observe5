3e50c7a37d6e79f42a7224b0104cd1f47b2e1f18
caine <luojiong@mooe.robot.com>
2023-07-10 13:39:23 +0800

    '[feat]{initState}'

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,58 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
+import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
+import { visualizer } from "rollup-plugin-visualizer";
 import progress from "rollup-plugin-progress";
 
 const BabelExcludes = ["./node_modules/**"];
 export default {
-  input: "./src/index.ts",
+  input: "WeakMapEmitter.ts",
   output: [
     {
-      name: "weakMapEmitter",
-      dir: "./build",
+      name: "WeakMapEmitter",
+      file:"./build/weakMapEmitter.esm.js",
       format: "esm",
-      preserveModules: true,
-      preserveModulesRoot: "./WeakMapEmitter.ts",
-      plugins: [process.env.NODE_ENV === "production" && terser()],
+      plugins:[terser()]
+    },
+    {
+      name: "WeakMapEmitter",
+      file:"./build/weakMapEmitter.cjs.js",
+      format: "cjs",
+      plugins:[terser()]
     },
   ],
-  sourcemap: false,
+  sourcemap: process.env.NODE_ENV === "production" ? false : "inline",
   plugins: [
+    resolve(),
+    commonjs(),
     typescript({
       lib: ["es5", "es6", "dom"],
       tsconfig: "./tsconfig.json",
-      target: "es6",
+      target: "es5",
       exclude: BabelExcludes,
     }),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      extensions: [".ts", ".js"],
+      presets: ["@babel/preset-env", "@babel/preset-typescript"],
+    }),
+    replace({
+      exclude: "node_modules/**",
+      preventAssignment: true,
+      ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+    }),
     progress({
-        clearLine: true, // default: true
-      }),
+      clearLine: true, // default: true
+    }),
+    visualizer({
+      emitFile: true,
+      dir:"./",
+      filename: "stats.html",
+    }),
   ],
   exclude: ["./node_modules/**"],
 };
